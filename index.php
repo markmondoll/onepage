db0c7983f307ae3acc5a2f9582aa0737a3d42a7b
markmondoll <markmondol@gmail.com>
2020-11-03 23:08:37 +0600

    style.css file added

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Landing Page</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+		<h1>Welcome to Landing Page Website</h1>
+<!--Hero Section-->		
+<section id=hero>
+	<div class="hero container">
+		<div>
+			<h1>Hello, My Name is Mark Mondoll</h1>
+			<a href="#" type="button" class="cta">Portfolio</a>
+		</div>
+		
+	</div>
+	
+</section>
+	<!--Header Section-->		
+
+	<!--End Header Section-->		
+
+	<!--Logo Section-->		
+
+	<!--End Logo Section-->		
+
+	<!--Menu Section-->		
+
+	<!--End Menu Section-->		
+
+<!--End Hero Section-->		
+
+
+<!--Login Section-->		
+
+<!--End Login Section-->		
+
+<!--Service Section-->		
+
+<!--End Service Section-->	
+
+<!--Registration Section-->		
+
+<!--End Registration Section-->		
+
+<!--Employee Section-->		
+
+<!--End Employee Section-->		
+
+<!--Contact Us Section-->		
+
+<!--End Contact Us Section-->		
+
+<!--Footer Section-->		
+
+<!--End Footer Section-->		
+
+<!--Copyright Section-->		
+
+<!--End Copyright Section-->	
+
+</body>
+</html>
